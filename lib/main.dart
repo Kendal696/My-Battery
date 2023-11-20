ebd5f32cc53ec2c5fa5abd5206cc1ed0bdd94560
Kendal696 <kevindaniellozano7@gmail.com>
2023-11-20 08:44:49 -0400

    Nueva interfaz

--- lib/main.dart
+++ lib/main.dart
@@ -10,55 +10,135 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Battery Percentage'),
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.lightBlueAccent, Colors.white],
-            ),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/battery_icon.png', // Ruta de tu imagen
-                  width: 100,
-                  height: 100,
-                ),
-                SizedBox(height: 20),
-                StreamBuilder<AndroidBatteryInfo?>(
-                  stream: BatteryInfoPlugin().androidBatteryInfoStream,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      final batteryLevel =
-                          snapshot.data?.batteryLevel ?? 'Desconocido';
-                      return Card(
-                        elevation: 4,
-                        color: Colors.lightBlueAccent, // Color celeste
-                        child: Padding(
-                          padding: EdgeInsets.all(16),
-                          child: Text(
-                            "Battery Level: $batteryLevel%",
-                            style: TextStyle(fontSize: 24),
-                          ),
-                        ),
-                      );
-                    } else {
-                      return CircularProgressIndicator();
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
         ),
       ),
+      home: BatteryMonitorPage(),
+    );
+  }
+}
+
+class BatteryMonitorPage extends StatefulWidget {
+  @override
+  _BatteryMonitorPageState createState() => _BatteryMonitorPageState();
+}
+
+class _BatteryMonitorPageState extends State<BatteryMonitorPage> {
+  double? _batteryLevel;
+
+  @override
+  void initState() {
+    super.initState();
+    BatteryInfoPlugin().androidBatteryInfoStream.listen((batteryInfo) {
+      setState(() {
+        _batteryLevel = batteryInfo?.batteryLevel?.toDouble();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Battery Monitor'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.white, Colors.white],
+          ),
+        ),
+        child: ListView(
+          children: [
+            BatteryLevelSection(batteryLevel: _batteryLevel),
+            Divider(),
+            BatteryHistorySection(),
+            Divider(),
+            BatteryAlertsSection(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BatteryLevelSection extends StatelessWidget {
+  final double? batteryLevel;
+
+  const BatteryLevelSection({Key? key, this.batteryLevel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.battery_charging_full,
+            size: 100,
+            color: Colors.blue,
+          ),
+          SizedBox(height: 20),
+          batteryLevel != null
+              ? BatteryLevelCard(batteryLevel: batteryLevel!)
+              : CircularProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class BatteryLevelCard extends StatelessWidget {
+  final double batteryLevel;
+
+  const BatteryLevelCard({Key? key, required this.batteryLevel})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      color: Colors.redAccent,
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Text(
+          "Battery Level: ${batteryLevel.toStringAsFixed(0)}%",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+// Widget para la sección de Historial de Batería
+class BatteryHistorySection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.history, color: Colors.blue),
+      title: Text('Battery History'),
+      onTap: () {
+        // TODO: Navegar a la pantalla de historial de batería
+      },
+    );
+  }
+}
+
+// Widget para la sección de Alertas de Nivel de Batería
+class BatteryAlertsSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.notifications_active, color: Colors.red),
+      title: Text('Battery Alerts'),
+      onTap: () {
+        // TODO: Navegar a la pantalla de configuración de alertas
+      },
     );
   }
 }
